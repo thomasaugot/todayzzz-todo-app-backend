18bd9ff092db187b96442c971e0d4dced5dda5cd
THOMAS AUGOT <thomas.augot@hotmail.fr>
2024-09-24 16:36:46 +0100

    fixing cors policies

--- server.js
+++ server.js
@@ -1,19 +1,36 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
 import authRoutes from "./routes/authRoutes.js";
 import todoRoutes from "./routes/todoRoutes.js";
 import collectionsRoutes from "./routes/collectionRoutes.js";
-import dotenv from "dotenv";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middleware
-app.use(cors());
+// Allowed origins
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://todayzzz-todos.netlify.app",
+];
+
+// Configure CORS
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
-// Routes
 app.use("/api/auth", authRoutes);
 app.use("/api", todoRoutes);
 app.use("/api", collectionsRoutes);
